6a63f83dff7fdaf0fe89f3ca580705fb17ab54cb
Ryan D <rdeber@gmail.com>
2018-06-02 23:06:54 -0400

    Working on about page content.

--- pages/about.vue
+++ pages/about.vue
@@ -1,24 +1,32 @@
 <template>
   <section class="section is-medium">
     <div class="container has-text-centered">
-      <h1><i class="fas fa-user-ninja"></i>Who am I</h1>
+      <h1><i class="fas fa-user-ninja"></i>Who am I?</h1>
+      <p>Hello, my name is Ryan DeBerardinis and I am a creative professional living and working in New York City. I specialize in front end development and UI design, with a passion for photography. Over the years I’ve worked as both a freelancer and as an integral part of dynamic teams. I’ve had the privilege to work with some amazing companies and collaborate on some truly inspiring projects. My photography has also been featured in many articles, publications and websites.</p>
       <div class="columns">
-        <div class="column">
-          <i class="fas fa-paint-brush"></i>
-          <h2>Designer</h2>
-          <p>This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rule in my opionion.</p>
-        </div>
         <div class="column">
           <i class="fas fa-code"></i>
           <h2>Developer</h2>
-          <p>This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rule in my opionion.</p>
+          <p>Over the years I’ve worked on a multitude of front-end projects for a wide variety of clients. I’m skilled at delivering streamlined websites and UI experiences using optimized and efficient code. In my work I utilize an assortment of the latest tools and languages, including HTML, CSS, JS, SCSS, git, bootstrap, wordpress, etc..</p>
+        </div>
+        <div class="column">
+          <i class="fas fa-paint-brush"></i>
+          <h2>Designer</h2>
+          <p>Inspired by a minimalist approach my design work focuses on creating beautiful and inspiring creations that maximize usability. I have over 15 years industry experience designing beautiful things that utilize the latest technologies for a wide variety of clients. </p>
         </div>
         <div class="column">
           <i class="fas fa-camera"></i>
           <h2>Photographer</h2>
-          <p>This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rule in my opionion.</p>
+          <p>My photography focuses on a range of subjects from grand landscapes to intimate urban city scenes. I began my love of photography studying traditional darkroom techniques and alternative photographic process years ago. Today I practice the latest digital photographic methods through my ongoing exploration of the streets of New York City.</p>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<style scoped>
+.svg-inline--fa {
+  font-size: 4rem;
+}
+
+</style>
